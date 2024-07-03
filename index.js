89a0edf333c93ecb2880ac2961d9c7ae75bd81e3
Ahmed Essam El Sayed <ahmedessaam124@gmail.com>
2024-07-03 21:41:25 +0300

    Finish Html and Css Files

---
+++ index.js
@@ -0,0 +1,25 @@
+let YearsInput = document.getElementById("yearinputs");
+let MonthsInput = document.getElementById("monthinputs");
+let DaysInput = document.getElementById("daysinputs");
+
+let YearsResult = document.getElementById("outY");
+let MonthsResult = document.getElementById("outMo");
+let DaysResult = document.getElementById("outDay");
+
+function Calc() {
+    let date = new Date();
+    let currentDay = date.getDate() ;
+    let currentMonth = date.getMonth() + 1 ;
+    let currentYear = date.getFullYear() ;
+    let All_Moths = [31 , 28 , 31 , 30 , 31 , 30 , 31 , 31 ,30 , 31 , 30 ,31];
+    
+
+
+
+
+
+
+
+
+
+}
