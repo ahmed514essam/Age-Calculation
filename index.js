6ce1c94b661b0e9aa8a21aa8ae889113a61f34e3
Ahmed Essam El Sayed <ahmedessaam124@gmail.com>
2024-07-05 02:39:02 +0300

    CalcAgeIsFinish

--- index.js
+++ index.js
@@ -7,19 +7,30 @@ let MonthsResult = document.getElementById("outMo");
 let DaysResult = document.getElementById("outDay");
 
 function Calc() {
-    let date = new Date();
-    let currentDay = date.getDate() ;
-    let currentMonth = date.getMonth() + 1 ;
-    let currentYear = date.getFullYear() ;
-    let All_Moths = [31 , 28 , 31 , 30 , 31 , 30 , 31 , 31 ,30 , 31 , 30 ,31];
-    
+  let day = DaysInput.value;
+  let month = MonthsInput.value;
+  let year = YearsInput.value;
 
+  let date = new Date();
+  let currentDay = date.getDate();
+  let currentMonth = date.getMonth() + 1;
+  let currentYear = date.getFullYear();
+  let All_Moths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
+  if (day > currentDay) {
+    currentDay = currentDay + All_Moths[currentMonth - 1];
+    currentMonth = currentMonth + 12;
+  }
+  if (month > currentMonth) {
+    currentMonth = currentMonth + 12;
+    currentYear = currentYear - 1;
+  }
 
+  let dd = currentDay - day;
+  let mm = currentMonth - month;
+  let yy = currentYear - year;
 
-
-
-
-
-
+  YearsResult.innerHTML = yy;
+  MonthsResult.innerHTML = mm;
+  DaysResult.innerHTML = dd;
 }
